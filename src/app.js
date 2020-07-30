fc3bd0bbd2747ee9890693275bc5f03289b8a3ac
Al Diaz <mypersonalwebsite@outlook.com>
2020-07-30 15:24:02 -0700

    adding map and basic grid

--- src/app.js
+++ src/app.js
@@ -49,6 +49,13 @@ console.log(human)
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
+function compareWeight (dino, human) {
+  if (dino.weight > human.weight) {
+    console.log(`This dino is ${dino.weight - human.weight} lbs weighter than the human`)
+  } else {
+    console.log('We have a Super Human!')
+  }
+}
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
@@ -57,6 +64,22 @@ console.log(human)
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
 // Generate Tiles for each Dino in Array
+function grid (arr, human) {
+
+  const arrMiddle = Math.floor(arr.length / 2)
+  let arrWithHuman = [...arr.slice(0, arrMiddle), human, ...arr.slice(arrMiddle)]
+  
+  console.log(arrWithHuman)
+
+
+  let dinosGrid = ''
+  arrWithHuman.map((el, index) => {
+    console.log(el)
+    if (index !== 0 && index % 3 === 0) { dinosGrid += '<br>' }
+    dinosGrid += `<span class='grid'>${el.species}</span>`
+  })
+  return dinosGrid
+}
 
 // Add tiles to DOM
 
@@ -66,6 +89,13 @@ const btnCompare = document.querySelector('.compare')
 btnCompare.addEventListener('click', () => {
   console.log('Button Compare clicked.')
   document.querySelector('#container').classList.add('hide')
+
+  const wrapper = document.querySelector('.form-container')
+  let dinoLayout = document.createElement('div')
+  dinoLayout.classList.add('dino-grid')
+  dinoLayout.innerHTML = grid(Dinos, { species: 'Human' })
+  wrapper.appendChild(dinoLayout)
+
   createButton('div', 'Back!', ['btn', 'goBack'], 'form-container', 'container')
 })
 
@@ -87,13 +117,14 @@ function listenerForButton (targetClass, elementToShowID) {
   console.log('Button Back clicked.')
   document.querySelector(`#${elementToShowID}`).classList.remove('hide')
 
+  const grid = document.querySelector('.dino-grid')
+  grid.remove()
+
   // when we remove an element with a listener we have to remove the listener as well
   const btn = document.querySelector(`.${targetClass}`)
   btn.removeEventListener('click', listenerForButton, false)
   btn.remove()
 }
-
-
 
 
 // On button click, prepare and display infographic
