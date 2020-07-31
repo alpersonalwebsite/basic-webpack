7f6d1a443d136d2c5a746d753da71c9dd427e565
Al Diaz <mypersonalwebsite@outlook.com>
2020-07-31 12:22:36 -0700

    refactoring and adding methods

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ require('./index.html');
 
 import { dinos, compareWeight, compareHeight, compareDiet } from './dino.js';
 import { Human } from './human.js';
-import { shapeData, generateRandomIndexes, showComparison } from './helpers.js';
+import {
+  shapeData,
+  replaceFacts,
+  generateRandomIndexes,
+  showComparison
+} from './helpers.js';
 
 import anklyosaurus from './images/anklyosaurus.png';
 import brachiosaurus from './images/brachiosaurus.png';
@@ -15,8 +20,8 @@ import stegosaurus from './images/stegosaurus.png';
 import triceratops from './images/triceratops.png';
 import tyrannosaurusrex from './images/tyrannosaurus rex.png';
 
-
-let transformedDinosArr = shapeData(dinos, { species: 'Human' })
+let transformedDinosArr = shapeData(dinos, { species: 'Human', weight: 200 });
+let arrBeforeFactMutation = [...transformedDinosArr]
 
 // Use IIFE to get human data from form
 
@@ -36,16 +41,43 @@ let humanOb = new Human(humanDataObj);
 console.log(humanOb);
 
 
-generateRandomIndexes(transformedDinosArr, 3)
-
 
 // Remove form from screen and Add tiles to DOM
 const btnCompare = document.querySelector('.compare');
 
 btnCompare.addEventListener('click', () => {
-  showComparison(transformedDinosArr, 'container', 'form-container')
+
+  generateRandomIndexes(transformedDinosArr, 3);
+
+  const indexesToReplaceWithFacts = generateRandomIndexes(transformedDinosArr, 3);
+
+  let weightComparison = compareWeight(
+    transformedDinosArr[indexesToReplaceWithFacts[0]],
+    { species: 'Human', weight: 200 }
+  );
+  console.log(weightComparison);
+
+  let heightComparison = compareHeight(
+    transformedDinosArr[indexesToReplaceWithFacts[1]],
+    { species: 'Human', weight: 200 }
+  );
+
+  let dietComparison = compareDiet(
+    transformedDinosArr[indexesToReplaceWithFacts[2]],
+    { species: 'Human', weight: 200 }
+  );
+
+  let newArrWithFacts = replaceFacts(
+    transformedDinosArr,
+    indexesToReplaceWithFacts,
+    [weightComparison, heightComparison, dietComparison]
+  );
+
+  showComparison(arrBeforeFactMutation, newArrWithFacts, 'container', 'form-container', {
+    element: 'div',
+    text: 'Back!',
+    classes: ['btn', 'goBack']
+  });
 });
-
-
 
 // On button click, prepare and display infographic
--- src/helpers.js
+++ src/helpers.js
@@ -20,6 +20,22 @@ export function shapeData(originalDinoArr, humanOb) {
   return arrWithHuman;
 }
 
+export function replaceFacts(originalArr, indexesToReplaceArr, newFacts) {
+  const newArrWithModifiedFacts = originalArr.map(obj => Object.assign({}, obj));
+  console.log('indexesToReplaceArr', indexesToReplaceArr)
+  for (let i = 0; i < indexesToReplaceArr.length; i++) {
+    //console.log(newArrWithModifiedFacts[indexesToReplaceArr[i]])
+    newArrWithModifiedFacts[indexesToReplaceArr[i]].fact = `${newFacts[i]}`
+  }
+  return newArrWithModifiedFacts
+}
+
+export function restoreFacts(originalArr) {
+  console.log('original',originalArr)
+  return originalArr
+}
+
+
 // We are going to use this map to target human and bid indexes which we will use to exclude in random
 function getHumanAndBirdIndexes(arr) {
   let humanIndex, birdIndex;
@@ -35,25 +51,52 @@ function getHumanAndBirdIndexes(arr) {
 }
 
 // Random
+function randomNumberWithRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export function generateRandomIndexes(arr, gridLength) {
   // Random comparison
   let indexesToExclude = getHumanAndBirdIndexes(arr);
+  indexesToExclude = Object.values(indexesToExclude);
+  console.log(indexesToExclude);
 
   console.log(arr);
 
   // Calculate "rows"
-  let gridRows = arr.length / gridLength;
-  console.log(gridRows);
+  let gridRows = Math.ceil(7 / gridLength);
+  let lastRowElements = 7 % gridLength;
 
-  let ranDomNumberForIndex = Math.floor(Math.random() * (arr.length + 1));
-  if (
-    ranDomNumberForIndex === indexesToExclude.human &&
-    ranDomNumberForIndex === indexesToExclude.bird
-  )
-    ranDomNumberForIndex = Math.floor(Math.random() * (arr.length + 1));
+  console.log(gridRows, lastRowElements);
 
-  console.log(ranDomNumberForIndex);
-  return ranDomNumberForIndex;
+  // I want to ensure I have one random fact per row
+  let validIndexes = [];
+  for (let i = 1; i <= gridRows; i++) {
+    let start = (i - 1) * gridLength;
+    let end =
+      i === gridRows ? start + lastRowElements : start + (gridLength - 1);
+    //let range = [start, end];
+    let randomIndexPerRow = randomNumberWithRange(start, end);
+    //console.log('Is index in range', indexesToExclude.indexOf(randomIndexPerRow), randomIndexPerRow)
+    while (indexesToExclude.indexOf(randomIndexPerRow) === 1)
+      randomIndexPerRow = randomNumberWithRange(start, end);
+    validIndexes.push(randomIndexPerRow);
+    // console.log('Range', range)
+    // console.log('RandomIndex', randomIndexPerRow)
+  }
+
+  console.log('valid', validIndexes);
+
+  // let ranDomNumberForIndex = Math.floor(Math.random() * (arr.length + 1));
+  // if (
+  //   ranDomNumberForIndex === indexesToExclude.human &&
+  //   ranDomNumberForIndex === indexesToExclude.bird
+  // )
+  //   ranDomNumberForIndex = Math.floor(Math.random() * (arr.length + 1));
+
+  // console.log(ranDomNumberForIndex);
+  // return ranDomNumberForIndex;
+  return validIndexes;
 
   // // Comparison 1
   // arrWithHuman[ranDomNumberForIndex].fact = compareWeight(
@@ -64,7 +107,7 @@ export function generateRandomIndexes(arr, gridLength) {
 
 // Layout
 // Generate Tiles for each Dino in Array
-function grid (arr) {
+function grid(arr) {
   let dinosGrid = '';
   arr.map((el, index) => {
     console.log(el);
@@ -80,27 +123,27 @@ function grid (arr) {
   return dinosGrid;
 }
 
-export function showComparison (dinosArr, formWrapperId, containerClass) {
+export function showComparison(
+  prevDinosArr,
+  dinosArrWithFacts,
+  formWrapperId,
+  containerClass,
+  { element, text, classes }
+) {
   console.log('Button Compare clicked.');
   document.querySelector(`#${formWrapperId}`).classList.add('hide');
 
   const wrapper = document.querySelector(`.${containerClass}`);
   let dinoLayout = document.createElement('div');
   dinoLayout.classList.add('dino-grid');
-  dinoLayout.innerHTML = grid(dinosArr, { species: 'Human', weight: 200 });
+  dinoLayout.innerHTML = grid(dinosArrWithFacts, { species: 'Human', weight: 200 });
   wrapper.appendChild(dinoLayout);
 
-  createButton(
-    'div',
-    'Back!',
-    ['btn', 'goBack'],
-    'form-container',
-    'container'
-  );
+  createButton(element, text, classes, containerClass, formWrapperId, prevDinosArr);
 }
 
 // Yes, I could have a button in the HTML template and toggle the class, but... I wanted to practice JS
-function createButton (wrapper, text, cssClass, parentWrapper, elementToShowID) {
+function createButton(wrapper, text, cssClass, parentWrapper, elementToShowID, prevDinosArr) {
   const btn = document.createElement(wrapper);
   const btnContent = document.createTextNode(text);
   btn.appendChild(btnContent);
@@ -110,13 +153,15 @@ function createButton (wrapper, text, cssClass, parentWrapper, elementToShowID) 
   positionTarget.appendChild(btn);
 
   // we expect the last class is the one we are going to use to identify the element
-  btn.addEventListener('click', () =>
+  btn.addEventListener('click', () => {
     listenerForButton(cssClass[cssClass.length - 1], elementToShowID)
+    restoreFacts(prevDinosArr)
+  }
+
   );
 }
 
-
-function listenerForButton (targetClass, elementToShowID) {
+function listenerForButton(targetClass, elementToShowID) {
   console.log('Button Back clicked.');
   document.querySelector(`#${elementToShowID}`).classList.remove('hide');
 
